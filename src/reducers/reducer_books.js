80761f14a672ab51241d2a6330c39196d0d720e8
Mark Karvonen <markkarvonen@MacBook-Pro-3.local>
2018-01-05 14:16:36 -0500

    book store done

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'js 1', pages: 1},
+        { title: 'js 2', pages: 2},
+        { title: 'js 3', pages: 3},
+        { title: 'js 4', pages: 4}
+    ]
+}
